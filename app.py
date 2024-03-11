b14f8cc8afe0946ee19a51bd11969c0ceca7303c
Khadija <khadija.taquee@gmail.com>
2024-03-11 14:11:59 -0500

    Set up basic flask application

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask, jsonify, request
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Fuel Quote Server is up and running!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
