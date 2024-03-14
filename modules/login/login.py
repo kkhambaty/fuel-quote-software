8de8a77ec29adbd2c783098eea86700436ece85d
Bioluminescent16 <103149841+Bioluminescent16@users.noreply.github.com>
2024-03-13 19:38:44 -0500

    Potential database mockup?

--- modules/login/login.py
+++ modules/login/login.py
@@ -1,2 +1,9 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
 
+login_bp = Blueprint('login', __name__, url_prefix='/login')
+
+#for now, mock user data
+users = {
+    'user1': {'username': 'user1', 'password': 'password1'},
+    'user2': {'username': 'user2', 'password': 'password2'}
+}
