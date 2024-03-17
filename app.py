8ad10d20f7679f00da8f484564fc3894fecaa358
Khadija <khadija.taquee@gmail.com>
2024-03-17 15:29:05 -0500

    registered profile blueprint

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from modules.profile import profile_bp 
 
 app = Flask(__name__)
 
@@ -7,4 +8,6 @@ def home():
     return 'Fuel Quote Server is up and running!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=True)
+
+app.register_blueprint(profile_bp, url_prefix='/profile')
