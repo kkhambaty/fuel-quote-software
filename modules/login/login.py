afab1e89c35d2f137ba82789cf46b69a0cb4ff59
Bioluminescent16 <103149841+Bioluminescent16@users.noreply.github.com>
2024-04-12 10:42:59 -0500

    Update

--- modules/login/login.py
+++ modules/login/login.py
@@ -5,10 +5,10 @@ from flask import Blueprint, Flask, request, jsonify
 login_bp = Blueprint('log-in', __name__)
 
 # Mock user data (replace this with a database later)
-users = {
-    'user1': {'username': 'user1', 'password': 'password1'},
-    'user2': {'username': 'user2', 'password': 'password2'}
-}
+#users = {
+#    'user1': {'username': 'user1', 'password': 'password1'},
+#    'user2': {'username': 'user2', 'password': 'password2'}
+#}
 
 # Login route with validations
 @login_bp.route('/login', methods=['POST'])
